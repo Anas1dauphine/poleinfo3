600b178b5be763d782509c2a910936506f805636
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2017-01-23 16:08:57 +0100

    Rm redundant file

--- true_DBInfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Constants that define how to connect the DB
-
-// The standard user: can do anything 
-  define ("NAME","adminMaster");
-  define ("PASS", "MODIFIED-Olivier");
-  define ("SERVER", "localhost");
-  define ("BASE", "Master");
-?>
